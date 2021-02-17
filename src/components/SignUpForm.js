1fa5a63912d2f49a20dbdd22a6d660d84dea4f57
Jared <jlong4223@gmail.com>
2021-02-17 14:04:43 -0600

    sets up signup form component

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,14 @@
+import SignUpPage from "../pages/SignUpPage";
+
+const SignUpForm = (props) => {
+  return (
+    <div>
+      <h1>SignUp Form</h1>
+      <form>
+        <legend>SignUp Below:</legend>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
