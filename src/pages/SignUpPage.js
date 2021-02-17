82212ebe829a6397e65af6d65a307284aca75bac
Jared <jlong4223@gmail.com>
2021-02-17 14:04:56 -0600

    imports form into sign up page

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import SignUpForm from "../components/SignUpForm";
+
+const SignUpPage = (props) => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUpPage;
