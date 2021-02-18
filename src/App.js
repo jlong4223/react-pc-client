918ae8130ab5791bd1bbc3158e4ee50dfb5fb8bd
Jared <jlong4223@gmail.com>
2021-02-17 21:56:41 -0600

    creates login route and passes props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,41 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import { getUser, logout } from "./services/UserService";
 
 function App() {
+  const [userState, setUserState] = useState({ user: getUser() });
+
+  // TODO figure out why a user is not being grabbed from localstorage after logging in
+  function handleSignupOrLogin() {
+    setUserState({ user: getUser() });
+  }
+
   return (
     <div className="App">
       <Header />
       <Route
         exact
         path="/signup"
-        render={(props) => <SignUpPage {...props} history={props.history} />}
+        render={(props) => (
+          <SignUpPage
+            {...props}
+            history={props.history}
+            handleSignupOrLogin={handleSignupOrLogin}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/login"
+        render={(props) => (
+          <LoginPage
+            {...props}
+            history={props.history}
+            handleSignupOrLogin={handleSignupOrLogin}
+          />
+        )}
       />
       <Footer />
     </div>
