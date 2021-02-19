4c04f150d29463512344fb85e20087f5adf10e67
Jared <jlong4223@gmail.com>
2021-02-18 20:16:29 -0600

    sets up ternary to show name if there is a user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,20 @@
 const Header = (props) => {
-  return <header>Port Chaveriat</header>;
+  let nav = props.userState ? (
+    <div>
+      <p>Hi, {props.userState}!</p>
+    </div>
+  ) : (
+    <div>
+      <p>Please Login</p>
+    </div>
+  );
+
+  return (
+    <div>
+      <header>Port Chaveriat</header>
+      {nav}
+    </div>
+  );
 };
 
 export default Header;
