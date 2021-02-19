a24fd46c3bc624a44b9307168b4c52ce42158984
Jared <jlong4223@gmail.com>
2021-02-19 16:50:22 -0600

    adds header content and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,41 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import "./Header.css";
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+  height: 50px;
+  /* border: 1px solid green; */
+`;
 
 const Header = (props) => {
   let nav = props.userState ? (
-    <div>
-      <p>Hi, {props.userState}!</p>
+    <div className="nav">
+      <Link to="">Hi, {props.userState}!</Link>
       <Link to="" onClick={props.handleLogout}>
         Logout
       </Link>
     </div>
   ) : (
-    <div>
+    <div className="nav">
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
     </div>
   );
 
   return (
-    <div>
-      <header>Port Chaveriat</header>
-      {nav}
-    </div>
+    <StyledHeader>
+      <div>
+        <Link className="title" to="/">
+          <i className="fas fa-ship fa-lg"></i>
+          Port Chaveriat
+        </Link>
+      </div>
+      <div>{nav}</div>
+    </StyledHeader>
   );
 };
 
