fa74250e67dac3d7f7eba11e29eb9495fca0c062
Jared <jlong4223@gmail.com>
2021-02-19 16:49:28 -0600

    adds homepage directory, css file, and content to js

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <main className="home"></main>
+      <div>Home</div>
+    </>
+  );
+};
+
+export default HomePage;
