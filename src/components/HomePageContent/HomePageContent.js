bc43cc0b9182da0897f8ae54380b87988e9d8e8c
Jared Long <jlong4223@gmail.com>
2021-02-25 14:18:41 -0600

    links google maps and changes about info

--- src/components/HomePageContent/HomePageContent.js
+++ src/components/HomePageContent/HomePageContent.js
@@ -6,9 +6,9 @@ const HomePageContent = (props) => {
       <div className="contentDiv">
         <h4>Covid-19</h4>
         <p>
-          When visiting the park, it is required that visitors use face
-          coverings, practice social distancing and bring their own hand
-          sanitizer. The total number of park visitors may be limited.
+          When visiting, it is required that visitors use face coverings,
+          practice social distancing and bring their own hand sanitizer. The
+          total number of visitors may be limited.
         </p>
       </div>
       <div className="contentDiv" id="info">
