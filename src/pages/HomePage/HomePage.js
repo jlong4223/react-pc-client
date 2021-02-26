c3bf27494e96fae3376202167873b21755054388
Jared Long <jlong4223@gmail.com>
2021-02-26 16:52:07 -0600

    removes map and weather

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,22 @@
 import "./HomePage.css";
 import HomePageContent from "../../components/HomePageContent/HomePageContent";
 
+import Map from "../../components/Map/Map";
+import Weather from "../../components/Weather/Weather";
+
 const HomePage = (props) => {
+  // TODO pass props to homepage content and move map there
   return (
     <>
       <main className="main">
         <div className="home"></div>
       </main>
-      <HomePageContent />
+      <HomePageContent
+        lat={props.lat}
+        lng={props.lng}
+        icon={props.icon}
+        temp={props.temp}
+      />
     </>
   );
 };
