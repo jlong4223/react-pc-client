87f43fda13b2ae6b200d49ade47b8a5bfb7e02e8
Jared Long <jlong4223@gmail.com>
2021-02-28 21:02:28 -0600

    sets up state and uses fetched event data

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -2,14 +2,38 @@ import { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { fetchEventData } from "../services/Events";
+
 const localizer = momentLocalizer(moment);
 
 const SchedulePage = (props) => {
+  const [eventData, setEventData] = useState([
+    {
+      title: "",
+      start: null,
+      end: null,
+    },
+  ]);
+
+  useEffect(() => {
+    getEventData();
+  }, []);
+
+  async function getEventData() {
+    const data = await fetchEventData();
+    setEventData(data);
+  }
+
   const myEventsList = [
     {
       start: moment().toDate(),
       end: moment().add(1, "days").toDate(),
       title: "Some title",
+    },
+    {
+      start: moment().add(1, "days").toDate(),
+      end: moment().add(2, "days").toDate(),
+      title: "second title",
     },
   ];
 
