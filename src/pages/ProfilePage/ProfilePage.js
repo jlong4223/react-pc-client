a1dcc5bb3b773317aa490875eae86564c7e37abf
Jared Long <jlong4223@gmail.com>
2021-03-02 15:32:09 -0600

    adds profile pic from mysql

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,7 +1,10 @@
 const ProfilePage = (props) => {
   return (
     <div className="page">
-      <h1>Hi {props.user}</h1>
+      <div>
+        <h1 style={{ marginTop: 100 }}>Hi {props.user.user}</h1>
+        <img src={props.user.pic} alt="pic" style={{ width: 300 }}></img>
+      </div>
     </div>
   );
 };
