7dbee2facacac543c7b6076d25ddf7fecff1329b
Jared Long <jlong4223@gmail.com>
2021-03-01 20:19:37 -0600

    imports eventform and passes getEventData to the event form

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -3,6 +3,7 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { fetchEventData } from "../services/Events";
+import EventForm from ".././components/EventForm";
 
 const localizer = momentLocalizer(moment);
 
@@ -30,30 +31,16 @@ const SchedulePage = (props) => {
     return event;
   });
 
-  //   const myEventsList = [
-  //     {
-  //       start: moment().toDate(),
-  //       end: moment().add(1, "days").toDate(),
-  //       title: "Some title",
-  //     },
-  //     {
-  //       start: moment().add(1, "days").toDate(),
-  //       end: moment().add(2, "days").toDate(),
-  //       title: "second title",
-  //     },
-  //   ];
-
   return (
     <div>
       <Calendar
         localizer={localizer}
-        // events={myEventsList}
-        // events={eventData}
         events={allEvents}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500, marginBottom: 20 }}
       />
+      <EventForm getEventData={getEventData} />
     </div>
   );
 };
