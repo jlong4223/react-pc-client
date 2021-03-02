dd4e2d8b06e33b39fd2bb8c4d264eb332200349e
Jared Long <jlong4223@gmail.com>
2021-03-01 20:22:51 -0600

    sets up post function for events

--- src/services/Events.js
+++ src/services/Events.js
@@ -3,3 +3,14 @@ const BASE_URL = "http://localhost:3001/events";
 export function fetchEventData() {
   return fetch(BASE_URL).then((res) => res.json());
 }
+
+export function addEventData(event) {
+  return fetch(BASE_URL, {
+    body: JSON.stringify(event),
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+}
